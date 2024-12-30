acb551f2b10e383bfa1f5892da76de0dbc7c5185
Jean-Baptiste Camps <jbcamps@hotmail.com>
2024-12-30 11:37:42 +0100

    nearing 97%

--- main.py
+++ main.py
@@ -1,22 +1,42 @@
 import regex as re
+import pandas
+from Levenshtein import ratio
 
-consonant = "[bcdfghjklpqrstvwxzmn]"
+
+# List of issues
+# - final -ent (that can be unstressed or stressed)
+# - <ie> (diphtong, hiatus, …)
+
+consonant = "[bcçdfghjklpqrstvwxzmn]"
 nasal = "[mn]"
 vowel = "[aeiouyâêîôûàèùéëïü]"
 all = "[bcdfghjklpqrstvwxzmnaeiouyâêîôûàèùéëïü]"
+
+with open("fro/of3c_atone.tsv", "r") as f:
+    atone_words = [t.split('\t')[0] for t in f.readlines()]
 
 # Function to syllabify a word
 def syllabify(word):
     # Step 1: Mark consonant clusters (tr, pl, etc.) to prevent splitting
     word = re.sub(r'([tbgdpkfvc])([rl])', r'\1_\2', word)  # Temporarily replace "tr" or "pl" with "t_r", "p_l", etc.
     word = re.sub(r'(g)(n)', r'\1_\2', word)  # Temporarily replace "gn" "g_n"
+    word = re.sub(r'(c)(h)', r'\1_\2', word)  # Temporarily replace "ch" "c_h"
     # mark qu sequence
     word = re.sub(r'(q)(u)', r'\1_\2_', word)  # Temporarily replace "gn" "g_n"
+    ### Triphtongs
+    word = re.sub(r'([ie])(a[ul])', r'\1_\2', word)  # Temporarily replace "iau"/"eau", "i_a_u"
     # Step 1b: preserve diphtongs
     word = re.sub(r'([aeou])(i)', r'\1_\2', word)  # Temporarily replace "oi" with "o_i"
-    word = re.sub(r'([eo])(u)', r'\1_\2', word)
-    word = re.sub(r'([u])(e)', r'\1_\2', word)
-    word = re.sub(r'(i)([eéau])', r'\1_\2', word)  # Temporarily replace "oi" with "o_i"
+    word = re.sub(r'([aeo])(u)', r'\1_\2', word)
+    word = re.sub(r'(o)(e)(?=n)', r'\1_\2', word)
+    word = re.sub(r'([u])(e)(?=[^n])', r'\1_\2', word) #except verbal terminaisons. Better, look for tonic syllable?
+    word = re.sub(r'(i)([é])', r'\1_\2', word)  # Temporarily replace "ié" with "i_é"
+    #word = re.sub(r'(?<!'+vowel+'_?)'+'(i)([e])', r'\1_\2', word)  # Temporarily replace "ie" with "i_e", if not already preceded by two wowels
+    word = re.sub(r'(i)([e])(?=rs?$)', r'\1_\2', word) # suffix -ier
+    word = re.sub(r'(?<=(ch|g|j))(i)([e])', r'\1_\2', word) # Bartsch law
+    word = re.sub(r'(?<![_])(i)([e])(?=[nm](' + consonant + '|$))', r'\1_\2', word)  # nasal diphtong
+    word = re.sub(r'(i)([e])(?=[rls]'+consonant+')', r'\1_\2', word) # North-Eastern diphtongue conditionnée par r/l/s
+
 
     # My regex:
     # first, split between consonnants
@@ -38,7 +58,10 @@ def syllabify(word):
     return [s for s in syllables if s]
 
 # Examples to test the function
-words = ["battre", "batre", "mangier", "vëoir", "voit", "plaisir", "avoir", "dame", "chevalier",
+words = ["bielement", "maniere", "piece", "pieche", "iriez", "grieve", "liez", "mariage", "chastiaus", "poez", "demenoient", "boene",
+         "tierre",  "saluent", "mie", "escrie", "amie", "aie",
+         "peçoie", "salue", "battre", "batre", "mangier", "vëoir", "voit", "plaisir",
+         "avoir", "dame", "chevalier",
          "mangees", "Champaigne", "cherchier", "vialt", "outree", "deïst", "quanque"]
 
 for word in words:
@@ -48,36 +71,46 @@ for word in words:
 # Function to determine stress pattern for a word
 def get_stress(word, next_word=None):
     syllables = syllabify(word)
+
     if len(syllables) == 1:  # Monosyllabic word
-        return "S" if word not in ["que", "de", "le", "la", "et", "si"] else "w"
+        if word in atone_words:
+            return "w"
+        else:
+            return "S"
+
     else:
         # first, set all syllables as weak
         stress = ["w" for s in syllables]
-        if syllables[-1].endswith("e"):
-            stress[-2] = 'S'
-            if next_word is None or re.match(r"^"+vowel, next_word):
+        if re.match(".*es?$", syllables[-1]):
+            if word not in atone_words:
+                stress[-2] = 'S'
+            if next_word is None:
+                stress[-1] = "e" # elision at the end of the verse
+            elif syllables[-1].endswith("e") and (next_word is None or re.match(r"^"+vowel, next_word)):
                 stress[-1] = "e"
         else:
-            stress[-1] = "S"
-    return "".join(stress)
+            if word not in atone_words:
+                stress[-1] = "S"
+        return "".join(stress)
 
 # Function to process a line of verse
 def process_line(verse_num, line):
     # remove punctuation
-    line = re.sub(r"\p{P}", "", line)
+    line = re.sub(r"\p{P}", "", line).lower()
     words = line.split()
     result = []
     for i, word in enumerate(words):
         next_word = words[i + 1] if i + 1 < len(words) else None
         stress = get_stress(word, next_word=next_word)
         result.append(stress)
-    return f"{line} " + ";".join(result)
+    #return f"{line} " + ".".join(result)
+    return ".".join(result)
 
 # Test function for syllabification
 
 def syllabify_line(verse_num, line):
     #remove punctuation
-    line = re.sub(r"\p{P}", "", line)
+    line = re.sub(r"\p{P}", "", line).lower()
     words = line.split()
     result = []
     for i, word in enumerate(words):
@@ -106,3 +139,42 @@ for i, line in enumerate(lines, 1):
 # Process each line
 for i, line in enumerate(lines, 1):
     print(process_line(i, line))
+
+
+if __name__ == "__main__":
+
+    gt = pandas.read_csv("gt_fro.tsv", sep="\t")
+
+    lines = list(gt.Verse)
+
+    preds = []
+
+    for i, line in enumerate(lines, 1):
+        preds.append(process_line(i, line))
+
+
+    # EVAL scores
+    evals = [ratio(row["Annotation"], preds[index]) for index, row in gt.iterrows()]
+
+    pandas.DataFrame(evals).describe()
+
+    # print lines with errors
+    for index, row in gt.iterrows():
+        if evals[index] < 0.9:
+            print(str(index) + row["Verse"] + " --" + row["Annotation"] + " --" + preds[index] + " -- Score: " + str(evals[index]))
+
+    # print lines where syllable counts differ
+    for index, row in gt.iterrows():
+        if evals[index] < 1:
+            if len(row["Annotation"]) != len(preds[index]):
+                print(str(index) + row["Verse"] + " --" + row["Annotation"] + " --" + preds[index] + " -- Score: " + str(evals[index]))
+
+    # Compare with other tools
+    alternate = pandas.read_csv("gt_fro_with_preds.tsv", sep="\t")
+    evals = [ratio(row["Annotation"], row["mistral-large-2"]) for index, row in alternate.iterrows()]
+
+    pandas.DataFrame(evals).describe()
+
+
+    #ratio(list(gt.Annotation), preds)
+
